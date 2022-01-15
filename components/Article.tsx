c47374b3a9e716dd90cc691fdd88b3df20aa3c26
ricaza81 <ricaza81@gmail.com>
2022-01-14 19:14:27 -0500

    creation

--- components/Article.tsx
+++ components/Article.tsx
@@ -11,18 +11,15 @@ function Article(props) {
   return (
     <article>
       <a href={ base_url + props.article.permalink } target="_blank">
-
-      <div className="col-md-1">        
-          <img className="thumbnail" src={props.article.thumbnail}/>
-      </div>
-
-     <div className="col-md-3">
-        <h4>title: { props.article.title }</h4>
-        <h5># votes: { props.article.score }</h5>
-        <h5># comments: { props.article.num_comments }</h5>
-        <h6>Creation: { moment(new Date(props.article.created_utc * 1000).toISOString().replace('T',' ').slice(0,16)).fromNow()}</h6>
-     </div>
      
+         
+           <div className="col-md-3">
+           <span>
+           <img  src={props.article.thumbnail}/>
+           <h6>title: { props.article.title } | <br/> # votes: { props.article.score } | # comments: { props.article.num_comments } | Creation: { moment(new Date(props.article.created_utc * 1000).toISOString().replace('T',' ').slice(0,16)).fromNow()}</h6>
+           </span>
+           </div>
+    
       </a>
     </article>
   )
